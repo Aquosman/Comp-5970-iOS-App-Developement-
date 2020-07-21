6d4ee590f5eaa89f824250115cc54c3725f40ae5
Ian Fair <49998063+Aquosman@users.noreply.github.com>
2020-07-20 21:40:54 -0500

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  assignment1
+//
+//  Created by Ian Fair on 5/26/20.
+//  Copyright © 2020 Ian Fair. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("The first iPhone app by Ian Fair")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
